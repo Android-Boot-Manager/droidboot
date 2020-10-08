9d71e3692f4b83cc0360403a5ff0526741cdf474
luka177 <lukapanio@gmail.com>
2020-10-08 21:35:44 +0300

    It even boots

--- platform/lk/mtk/device.h
+++ platform/lk/mtk/device.h
@@ -1,7 +1,7 @@
 #include <lvgl/lvgl.h>
 void platform_sleep(int time);
-void platform disp_clear();
+void platform_disp_clear();
 bool platform_keyboard_read(lv_indev_drv_t * drv, lv_indev_data_t*data);
 void platform_fbcon_disp_flush(lv_disp_t * disp,
     const lv_area_t * area, lv_color_t * color_p);
-int platform_init();
+int platform_droidboot_init();
